71900382663841824a90782cab281ee669ba063f
xiaoyann <0x0886@gmail.com>
2016-06-15 15:02:58 +0800

    update example

--- Examples/Basic/app.js
+++ Examples/Basic/app.js
@@ -1,28 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
-import React, {
-    AppRegistry,
-    Component,
-    StyleSheet,
-    Text,
-    View
+import React, {Component} from 'react';
+import {
+    View, Text, StyleSheet, Modal, ListView
 } from 'react-native';
 
-import ActionSheet from 'react-native-actionsheet';
+import {ActionSheetCustom as ActionSheet} from 'react-native-actionsheet';
+// import ActionSheet from 'react-native-actionsheet';
 
-
-const buttons = ['取消', '确认退出', '😄😄😄', '哈哈哈'];
+const buttons = [
+    '取消', '确认退出', '😄😄😄', '哈哈哈',
+];
 const CANCEL_INDEX = 0;
 const DESTRUCTIVE_INDEX = 1;
 
+// console.log(Component)
+// export default function () { return null;}
 
-class RNActionSheet extends Component {
+class Child extends Component {
+    constructor(props) {
+        super(props);
+    }
 
     _handlePress(index) {
+        console.log('index: ', index);
     }
 
     show() {
@@ -31,7 +30,7 @@ class RNActionSheet extends Component {
 
     render() {
         return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <View style={styles.wrapper}>
                 <Text style={styles.button} onPress={this.show.bind(this)}>SHOW</Text>
 
                 <ActionSheet 
@@ -41,14 +40,47 @@ class RNActionSheet extends Component {
                     cancelButtonIndex={CANCEL_INDEX}
                     destructiveButtonIndex={DESTRUCTIVE_INDEX}
                     onPress={this._handlePress.bind(this)}
-                />
+                >
+                    <Text style={{color: 'red'}}>123</Text>
+                    <Text style={{color: 'red'}}>123</Text>
+                </ActionSheet>
+            </View>
+        );
+    }
+}
+
+var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+class RNActionSheet extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1, backgroundColor: '#fff', paddingBottom: 0}}>
+                <ListView
+                    dataSource={ds.cloneWithRows(['row 1', 'row 2'])}
+                    renderRow={(data) => <Child />}
+                >
+
+                </ListView>
+                <View style={{flex:1,height:200,backgroundColor:'blue'}}><Text>Bottom</Text></View>
             </View>
         );
     }
 }
 
 
+
 const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1, 
+        paddingBottom: 200, 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        backgroundColor: 'red'
+    },
     button: {
         width: 200,
         margin: 10,
--- Examples/Basic/index.android.js
+++ Examples/Basic/index.android.js
@@ -2,7 +2,6 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
-import React, {AppRegistry} from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './app';
 AppRegistry.registerComponent('Basic', () => App);
